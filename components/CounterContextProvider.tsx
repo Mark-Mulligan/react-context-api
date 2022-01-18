d0eefdc9f7178730275e286f562751c9673e5e1f
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-01-17 23:24:26 -0600

    modified counter

--- components/CounterContextProvider.tsx
+++ components/CounterContextProvider.tsx
@@ -22,7 +22,9 @@ const CounterContextProvider: FC = ({ children }) => {
   };
 
   const decrementCount = () => {
-    setCount(count - 1);
+    if (count > 0) {
+      setCount(count - 1);
+    }
   };
 
   return (
--- components/DecrementBtn.tsx
+++ components/DecrementBtn.tsx
@@ -4,7 +4,7 @@ import { CounterContext } from './CounterContextProvider';
 const DecrementBtn = () => {
   const { decrement } = useContext(CounterContext);
 
-  return <button onClick={decrement}>Increment</button>;
+  return <button onClick={decrement}>Decrement</button>;
 };
 
 export default DecrementBtn;
